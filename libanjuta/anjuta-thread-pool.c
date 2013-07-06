8de05197a94371d1703d5dfa17081a82291ff077
James Liggett <jrliggett@cox.net>
2013-07-06 14:37:25 -0700

    Fix e-mail addresses

--- libanjuta/anjuta-thread-pool.c
+++ libanjuta/anjuta-thread-pool.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
  * anjuta-thread-pool.c
- * Copyright (C) 2013 James Liggett <jim@jim-dekstop>
+ * Copyright (C) 2013 James Liggett <jrliggett@cox.net>
  *
  * anjuta is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
