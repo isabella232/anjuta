b1f4fdd6307d80e0131bcd788d26e54c88baefae
James Liggett <jrliggett@cox.net>
2013-08-09 20:52:04 -0700

    libanjuta/anjuta-command-bar: Don't use a shadow for the scrolled window

--- libanjuta/anjuta-command-bar.c
+++ libanjuta/anjuta-command-bar.c
@@ -173,8 +173,6 @@ anjuta_command_bar_add_action_group (AnjutaCommandBar *self,
 	scrolled_window = gtk_scrolled_window_new (NULL, NULL);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window),
 	                                GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
-	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scrolled_window),
-	                                     GTK_SHADOW_IN);
 
 	g_hash_table_insert (self->priv->widgets, (gchar *) group_name, 
 	                     scrolled_window);
