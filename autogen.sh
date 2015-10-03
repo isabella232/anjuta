9d07aabbad7ba3e2f114e24fa804f93f7867d01b
Sébastien Granjoux <seb.sfo@free.fr>
2015-10-03 14:21:19 +0200

    Check for yelp-tools to avoid an error later
    Makefile:628 *** missing separator

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
+
 test -z "$srcdir" && srcdir=.
 
+GNOMEDOC=`which yelp-build`
+if test -z $GNOMEDOC; then
+        echo "*** The tools to build the documentation are not found,"
+        echo "    please intall the yelp-tool package ***"
+        exit 1
+fi
+
+srcdir=`dirname $0`
 echo "Generating initial interface files"
 sh -c "cd $srcdir/libanjuta/interfaces && \
 perl anjuta-idl-compiler.pl libanjuta && \
